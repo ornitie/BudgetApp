ab02d9b6281fa4a103cc78da49114c1c2ffc8976
Juan Pablo Rodriguez Morales <pablo.rodriguez@mundi.io>
2023-04-17 20:46:32 -0500

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Juan Pablo Rodriguez Morales on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
